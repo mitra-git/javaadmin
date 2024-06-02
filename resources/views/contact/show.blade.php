4077ded3450d796bed6cceba0c2f1d5cc189eddf
SoffiaNoor <soffianaisyah0@gmail.com>
2024-06-02 10:59:50 +0700

    *add email input field

--- resources/views/contact/show.blade.php
+++ resources/views/contact/show.blade.php
@@ -53,6 +53,15 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group">
+                                        <label>Email</label>
+                                        <input type="email" id="email" name="email" class="form-control"
+                                            placeholder="Phone number" value="{{$contact->email}}" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
                                         <label>Message</label>
                                         <textarea rows="4" cols="80" class="form-control" id="message" name="message"
                                             placeholder="Here can be your description" disabled>{{$contact->message}}
